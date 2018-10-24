8e5e7360253baa1bcbe14aec337060a7d5ea45b4
muyan53 <534565377@qq.com>
2018-10-24 23:48:33 +0800

    first

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import App from './App.vue'
+// 引入路由模块
+import VueRouter from 'vue-router'
+// 引入index组件
+import index from './components/index.vue'
+
+// 使用路由中间件
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes:[
+    {
+      path:'/index',
+      component:index
+    }
+  ]
+})
+
+// 引入公共的css
+import './assets/statics/site/css/style.css';
+
+Vue.config.productionTip = false
+
+new Vue({
+  // 路由中间件挂载到vue
+  router,
+  // 渲染App组件
+  render: h => h(App)
+}).$mount('#app')
